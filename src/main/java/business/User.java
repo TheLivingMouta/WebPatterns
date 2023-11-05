cbefac745a081e737039c4ab4ee5f1f7508798ed
carol <zachaffron@yahoo.com>
2023-11-05 21:32:21 +0000

    JavaDocs for User Class and the creation of the UserRegistration application

--- src/main/java/business/User.java
+++ src/main/java/business/User.java
@@ -1,5 +1,12 @@
 package business;
 
+import java.util.Objects;
+
+/**
+ *
+ *
+ * @author Brian
+ */
 
 public class User {
 
@@ -11,10 +18,22 @@ public class User {
   private String firstname;
   private String lastname;
 
-
+  /**
+   * Default constructor
+   */
   public User() {
   }
 
+  /**
+   * Paramatised constructor
+   * @param id
+   * @param name
+   * @param email
+   * @param password
+   * @param phone
+   * @param firstname
+   * @param lastname
+   */
   public User(int id, String name, String email, String password, String phone, String firstname, String lastname) {
     this.id = id;
     this.name = name;
@@ -25,73 +44,155 @@ public class User {
     this.lastname = lastname;
   }
 
+
+  /**
+   * Get the user ID
+   * @return id
+   */
   public int getId() {
     return id;
   }
 
+  /**
+   *  sets the user id
+   * @param id
+   */
   public void setId(int id) {
     this.id = id;
   }
 
+  /**
+   * Gets the username of a user
+   * @return name
+   */
   public String getName() {
     return name;
   }
+
+  /**
+   * sets the username of a user
+   * @param name
+   */
 
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * gets the current email of a user
+   * @return
+   */
   public String getEmail() {
     return email;
   }
 
+  /**
+   * Sets the email of a user
+   * @param email
+   */
   public void setEmail(String email) {
     this.email = email;
   }
 
+  /**
+   * gets the password of a user
+   * @return password
+   */
   public String getPassword() {
     return password;
   }
 
+  /**
+   * sets the password of the user
+   * @param password
+   */
   public void setPassword(String password) {
     this.password = password;
   }
 
+  /**
+   * gets the phone number of a user
+   * @return
+   */
   public String getPhone() {
     return phone;
   }
 
+  /**
+   * sets the phone number of a user
+   * @param phone
+   */
   public void setPhone(String phone) {
     this.phone = phone;
   }
 
+  /**
+   * gets the first name of a user
+   * @return
+   */
   public String getFirstname() {
     return firstname;
   }
 
+  /**
+   * sets the first name of a user
+   * @param firstname
+   */
   public void setFirstname(String firstname) {
     this.firstname = firstname;
   }
 
+  /**
+   * gets the last name of a user
+   * @return
+   */
   public String getLastname() {
     return lastname;
   }
 
+  /**
+   * sets the last name of a user
+   * @param lastname
+   */
   public void setLastname(String lastname) {
     this.lastname = lastname;
   }
 
-  @Override
+  /**
+   * to string override
+   *
+   */
   public String toString() {
-    return "User{" + "id=" + id + ", name='" + name + '\'' + ", email='" + email + '\'' + ", password='" + password + '\'' + ", phone='" + phone + '\'' + ", firstname='" + firstname + '\'' + ", lastname='" + lastname + '\'' + '}';
+    return "User{" +
+            "Username=" + name +
+            ", User ID='" + id + '\'' +
+            ", Email=" + email +
+            ", phone='" + phone + '\'' +
+            ", First name=" + firstname +
+            ", Last name=" + lastname +
+            '}';
   }
 
+  /**
+   * Equals override
+   * @param object
+   * @return
+   */
   public boolean equals(Object object) {
     if (this == object) return true;
     if (object == null || getClass() != object.getClass()) return false;
     if (!super.equals(object)) return false;
     User users = (User) object;
-    return id == users.id;
+    return password == users.password;
+  }
+
+  /**
+   * HashCode Override
+   * @return
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(password);
   }
 }
 
